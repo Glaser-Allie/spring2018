1cf734f5fc451a84bc8eabc609ace32cd752132c
Glaser-Allie <gla13006@byui.edu>
2018-05-05 23:35:13 -0500

    more edits

--- web/index.php
+++ web/index.php
@@ -1,21 +1,27 @@
 <!DOCTYPE html>
 <!-- My site home page -->
 <html lang="en-us">
-  <head>
-    <?php include $_SERVER['DOCUMENT_ROOT'].'/modules/head.php'; ?>
-  </head>
 
-  <body>
-    <header>
-      <?php include $_SERVER['DOCUMENT_ROOT'].'/modules/header.php'; ?>
-    </header>
-    
-    <main>
-       
-    </main>
-    <footer>
-      <?php include $_SERVER['DOCUMENT_ROOT'].'/modules/footer.php'; ?>
-    </footer>
+<head>
+    <?php include $_SERVER['DOCUMENT_ROOT'].'/modules/head.php'; ?>
+</head>
+
+<body>
+    <section class="left-image"><img src="images/beach_042115.JPG"></section>
+    <section class="content-wrapper">
+        <header>
+            <?php include $_SERVER['DOCUMENT_ROOT'].'/modules/header.php'; ?>
+        </header>
+
+        <main>
+
+        </main>
+        <footer>
+            <?php include $_SERVER['DOCUMENT_ROOT'].'/modules/footer.php'; ?>
+        </footer>
+    </section>
     <script src="rotator/rotator.js"></script>
-  </body>
+
+</body>
+
 </html>
