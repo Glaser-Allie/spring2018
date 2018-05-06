31eda8f70f9154c77a22b27e7d0b6ed72b876938
Glaser-Allie <gla13006@byui.edu>
2018-05-05 22:55:41 -0500

    updated head

--- web/modules/head.php
+++ web/modules/head.php
@@ -2,4 +2,4 @@
 <title>GalvestonSandbox.com</title>
 <meta name="author" content="Allie Glaser">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" type="text/css" href="http://allieglaser.com/galveston/css/stylesheet.css"  media="screen">
+<link rel="stylesheet" type="text/css" href="/css/stylesheet.css"  media="screen">
